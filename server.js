96a09d99c9b77961db77d1497a2296bdbd7f35dd
Ken Ilo <keneilo91@yahoo.com>
2021-10-23 22:47:18 -0700

    added get routes to view html pages

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 
 const PORT = process.env.PORT || 3001;
 
+
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
 
