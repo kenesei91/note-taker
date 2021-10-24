a91dbf2700337a269b76f1fc9e2d69e8dd05c2d2
Ken Ilo <keneilo91@yahoo.com>
2021-10-24 00:21:36 -0700

    add task to app

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const savedNotes = require('./db/db.json');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
@@ -24,6 +26,21 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+function createNewNote(body, notesArray) {
+    const createdNotes = body;
+    notesArray.push(createdNotes);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return createdNotes;
+    //console.log(createdNotes);
+}
+
+app.post('/api/notes', (req, res) => {
+    const createdNotes = createNewNote(req.body, savedNotes);
+    res.json(createdNotes);
+})
 
 
 
